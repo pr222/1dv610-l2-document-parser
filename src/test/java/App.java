8c26d172992bd5d046944516418decac935f4250
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-10-18 20:02:14 +0200

    Setup project files and gradle

---
+++ src/test/java/App.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class App {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
